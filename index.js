92d8a361d991e867bb7c8d04ea156924327e34ee
Ajiboye <ajiboyemuyideen7@gmail.com>
2026-04-02 19:44:18 +0100

    binary search

--- index.js
+++ index.js
@@ -103,6 +103,7 @@ function binarySearch(arr, target) {
   let leftIndex = 0;
   let rightIndex = arr.length - 1;
 
+  
   while (leftIndex <= rightIndex) {
     let middleIndex = Math.floor((leftIndex + rightIndex) / 2);
 
