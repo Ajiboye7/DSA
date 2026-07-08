71d35c80a21456a25dabb015dd4e5c6ba8c57859
Ajiboye <ajiboyemuyideen7@gmail.com>
2026-07-08 19:08:28 +0100

    sum array

--- index.js
+++ index.js
@@ -147,30 +147,7 @@ function recursiveBinarySearch(
   }
 }
 
-//console.log(binarySearch([1, 2, 4, 5, 6], 4))
-
-//time complexity = o(log n): Each loop removes half the array.
-//space complexity = o(n) :  Call stack depth grows with n
-/*function bubbleSort(arr) {
-    let n = arr.length;
-
-    for (let i = 0; i < n; i++) {
-        for (let j = 0; j < n - i - 1; j++) { 
-            if (arr[j] > arr[j + 1]) {
-                // Swap elements
-                let temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-            }
-        }
-    }
-
-    return arr;
-}
-
-// Example usage
-let numbers = [5, 1, 4, 2, 8];
-console.log(bubbleSort(numbers));*/
+ 
 
 function sumArray(arr) {
   if (arr.length === 0) {
