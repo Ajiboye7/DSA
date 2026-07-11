f1c9f5995daa0edf4d9460992c3b1efa6da4e0a5
Ajiboye <ajiboyemuyideen7@gmail.com>
2026-07-11 10:40:40 +0100

    update

--- index.js
+++ index.js
@@ -169,7 +169,7 @@ function countVowels(str) {
   return (vowels.includes(firstChar) ? 1 : 0) + countVowels(str.slice(1));
 }
 
-//console.log(countVowels('ajiboye Muyideen olanrewaju Akanni Bolaji Olanrewaju'))
+//console.log(countVowels('ajiboye Muyideen olanrewaju'))
 
 function findMax(arr) {
   if (arr.length === 0) {
