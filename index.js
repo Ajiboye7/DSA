d08b124e63f67149e1cf1f2db3909969a055218f
Ajiboye <ajiboyemuyideen7@gmail.com>
2026-09-20 05:24:50 +0100

    tower of Hanoi

--- index.js
+++ index.js
@@ -388,7 +388,7 @@ function climbingStaircase(n) {
   return noOfways[n - 1];
 }
 
-//console.log(climbingStaircase(18))
+//console.log(climbingStaircase(17))
 
 function towerOfHanoi(n, source, auxiliary, destination) {
   if (n === 1) {
